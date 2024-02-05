74875cbf7ccaa746c8e6b774e3424928751c431e
mulugeta-89 <mulugetahail89@gmail.com>
2024-02-05 16:22:51 +0300

    Tasks component made

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button :style="{backgroundColor: color}" class="btn">{{text}}</button>
+
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        color: String
+    }
+}
+</script>
